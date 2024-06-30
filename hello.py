11644b6cb8e542185b5736bd50b706877a4b1673
socramwd <marcospg@outlook.es>
2024-06-30 07:45:22 +0200

    Termino el capítulo 2 de HelloPython

---
+++ hello.py
@@ -0,0 +1,22 @@
+# Esto es un comentario:
+# Nuestro Hola Mundo en Python
+print("Hola Python")
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+'''
+Este es un
+comentario
+en varias líneas
+'''
+
+# Consultar el tipo de dato
+print(type("Soy un dato string")) # Tipo de dato 'str'
+print(type(5)) # Tipo de dato 'int'
+print(type(1.5)) # Tipo de dato 'float'
+print(type(1 + 3j)) # Tipo de dato 'complex'
+print(type(True)) # Tipo de dato 'bool'
